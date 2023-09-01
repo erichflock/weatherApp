5c34f4f569762aa1ca0003d854aa83eb604a1cfd
Erich Flock <erich.flock@check24.de>
2023-09-01 17:34:18 +0200

    Create initial structure of the project

---
+++ weatherApp/weatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherAppApp.swift
+//  weatherApp
+//
+//  Created by Erich.Flock on 01.09.23.
+//
+
+import SwiftUI
+
+@main
+struct weatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
