1e85090b8a4f4ff812c2a4793827117a6f2ae768
Erich Flock <erich.flock@check24.de>
2023-09-01 21:23:01 +0200

    Implement initial layout of city view

---
+++ WeatherApp/CityView.swift
@@ -0,0 +1,57 @@
+//
+//  CityView.swift
+//  weatherApp
+//
+//  Created by Erich.Flock on 01.09.23.
+//
+
+import SwiftUI
+
+struct CityView: View {
+    
+    var body: some View {
+        VStack() {
+            mainContent
+                .padding(.bottom, 150)
+            Button("Search City", action: {
+                
+            })
+            .font(.system(size: 20))
+            .buttonStyle(.borderedProminent)
+        }
+    }
+    
+    private var mainContent: some View {
+        VStack(alignment: .center, spacing: 8) {
+            VStack(alignment: .leading) {
+                Text("Leipzig")
+                    .font(.largeTitle)
+                Text("30°")
+                    .font(.system(size: 80))
+            }
+            HStack {
+                VStack {
+                    Text("Rain")
+                        .font(.system(size: 20))
+                    Text("moderate rain")
+                        .font(.system(size: 16))
+                        .foregroundColor(.secondary)
+                }
+                Image(systemName: "cloud.rain.fill")
+                    .symbolRenderingMode(.palette)
+                    .foregroundStyle(.gray, .blue)
+                    .font(.system(size: 20))
+            }
+            HStack {
+                Text("H: 20°")
+                Text("L: 10°")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        CityView()
+    }
+}
