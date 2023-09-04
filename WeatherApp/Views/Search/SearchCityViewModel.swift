ce5c9205162856c9b3467f3c0a9d11bc1725d904
Erich Flock <erich.flock@check24.de>
2023-09-04 17:17:38 +0200

    Implement funcs for the SearchCityVM

--- WeatherApp/Views/Search/SearchCityViewModel.swift
+++ WeatherApp/Views/Search/SearchCityViewModel.swift
@@ -10,4 +10,36 @@ import SwiftUI
 final class SearchCityViewModel: ObservableObject {
     
     @Published var searchedCity: String = ""
+    @Published var cities: [City] = []
+    
+    private let geoCodingAPI: GeoCodingAPIProtocol
+    
+    init(geoCodingAPI: GeoCodingAPIProtocol = GeoCodingAPI()) {
+        self.geoCodingAPI = geoCodingAPI
+    }
+    
+    func fetchCities() async {
+        guard !searchedCity.isEmpty else { return }
+        do {
+            let apiCities = try await geoCodingAPI.getCities(for: self.searchedCity, limit: 5)
+            update(cities: City.mapper(apiCities: apiCities))
+        } catch {
+            update(cities: nil)
+        }
+    }
+    
+    func clearCities() {
+        update(cities: nil)
+    }
+    
+    private func update(cities: [City]?) {
+        guard let cities else {
+            self.cities = []
+            return
+        }
+        
+        DispatchQueue.main.async { [weak self] in
+            self?.cities = cities
+        }
+    }
 }
---
+++ WeatherAppTests/Views/Search/SearchCityViewModelTests.swift
@@ -0,0 +1,128 @@
+//
+//  SearchCityViewModelTests.swift
+//  WeatherAppTests
+//
+//  Created by Erich.Flock on 04.09.23.
+//
+
+import XCTest
+@testable import WeatherApp
+
+
+final class SearchCityViewModelTests: XCTestCase {
+
+    func test_clearCities_whenCalled_shouldRemoveCities() {
+        let sut = SearchCityViewModel(geoCodingAPI: GeoCodingAPISpy())
+        sut.cities = [.init(), .init()]
+        XCTAssertFalse(sut.cities.isEmpty, "precondition")
+        
+        sut.clearCities()
+        
+        XCTAssertTrue(sut.cities.isEmpty)
+    }
+    
+    func test_fethCities_whenSearchedCityEmpty_shouldNotCallGetCitiesOnGeoCodingAPI() async {
+        let geoCodingAPISpy = GeoCodingAPISpy()
+        let sut = SearchCityViewModel(geoCodingAPI: geoCodingAPISpy)
+        XCTAssertTrue(sut.searchedCity.isEmpty, "precondition")
+        XCTAssertEqual(geoCodingAPISpy.getCitiesCallCount, 0, "precondition")
+        
+        await sut.fetchCities()
+        
+        XCTAssertEqual(geoCodingAPISpy.getCitiesCallCount, 0)
+    }
+    
+    func test_fethCities_whenSearchedCityNotEmpty_shouldCallGetCitiesOnGeoCodingAPI() async {
+        let expectedReturnedApiCities: [GeoCodingCityAPIModel] = [.init(name: "London"),
+                                                                  .init(name: "City of London")]
+        let geoCodingAPISpy = GeoCodingAPISpy()
+        geoCodingAPISpy.apiCities = expectedReturnedApiCities
+        let sut = SearchCityViewModel(geoCodingAPI: geoCodingAPISpy)
+        sut.searchedCity = "London"
+        XCTAssertFalse(sut.searchedCity.isEmpty, "precondition")
+        XCTAssertEqual(geoCodingAPISpy.getCitiesCallCount, 0, "precondition")
+        
+        await sut.fetchCities()
+        
+        XCTAssertEqual(geoCodingAPISpy.getCitiesCallCount, 1)
+        XCTAssertEqual(geoCodingAPISpy.city, "London")
+        XCTAssertEqual(geoCodingAPISpy.apiCities, expectedReturnedApiCities)
+    }
+    
+    func test_fethCities_whenApiReturnsCities_shouldFulfillCities() async {
+        let expectedReturnedApiCities: [GeoCodingCityAPIModel] = [.init(name: "London"),
+                                                                  .init(name: "City of London")]
+        let geoCodingAPISpy = GeoCodingAPISpy()
+        geoCodingAPISpy.apiCities = expectedReturnedApiCities
+        let sut = SearchCityViewModel(geoCodingAPI: geoCodingAPISpy)
+        sut.searchedCity = "London"
+        XCTAssertFalse(sut.searchedCity.isEmpty, "precondition")
+        XCTAssertEqual(geoCodingAPISpy.getCitiesCallCount, 0, "precondition")
+        XCTAssertTrue(sut.cities.isEmpty, "precondition")
+        
+        await sut.fetchCities()
+        await addDelayToAllowCitiesToBeUpdated()
+        
+        XCTAssertEqual(geoCodingAPISpy.getCitiesCallCount, 1)
+        XCTAssertEqual(geoCodingAPISpy.apiCities, expectedReturnedApiCities)
+        XCTAssertEqual(sut.cities.count, expectedReturnedApiCities.count)
+        XCTAssertEqual(sut.cities[0].name, expectedReturnedApiCities[0].name)
+        XCTAssertEqual(sut.cities[1].name, expectedReturnedApiCities[1].name)
+    }
+    
+    func test_fethCities_whenThrowsError_shouldClearCities() async {
+        let geoCodingAPISpy = GeoCodingAPISpy()
+        geoCodingAPISpy.error = APIError.fetchError
+        let sut = SearchCityViewModel(geoCodingAPI: geoCodingAPISpy)
+        sut.searchedCity = "London"
+        sut.cities = [.init(), .init()]
+        XCTAssertFalse(sut.searchedCity.isEmpty, "precondition")
+        XCTAssertEqual(geoCodingAPISpy.getCitiesCallCount, 0, "precondition")
+        XCTAssertFalse(sut.cities.isEmpty, "precondition")
+
+        await sut.fetchCities()
+
+        XCTAssertEqual(geoCodingAPISpy.getCitiesCallCount, 1)
+        XCTAssertTrue(sut.cities.isEmpty)
+    }
+    
+}
+
+//MARK: Helpers
+extension SearchCityViewModelTests {
+    
+    private func addDelayToAllowCitiesToBeUpdated(_ delayInSeconds: Int = 1) async {
+        try? await Task.sleep(for: .seconds(delayInSeconds))
+    }
+    
+}
+
+private class GeoCodingAPISpy: GeoCodingAPIProtocol {
+    
+    var apiCities: [GeoCodingCityAPIModel]?
+    var error: Error?
+    private(set) var city: String?
+    private(set) var getCitiesCallCount = 0
+    
+    func getCities(for city: String, limit: Int) async throws -> [GeoCodingCityAPIModel]? {
+        getCitiesCallCount += 1
+        self.city = city
+        if let error {
+            throw error
+        }
+        return apiCities
+    }
+    
+}
+
+extension GeoCodingCityAPIModel: Equatable {
+    
+    public static func == (lhs: GeoCodingCityAPIModel, rhs: GeoCodingCityAPIModel) -> Bool {
+        lhs.name == rhs.name &&
+        lhs.lat == rhs.lat &&
+        lhs.lon == rhs.lon &&
+        lhs.country == rhs.country &&
+        lhs.state == rhs.state
+    }
+    
+}
